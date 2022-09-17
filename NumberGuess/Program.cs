a586401c2a6b8f79dad9ac6f6b3c2e0fd830ea38
Jalen Brown <jalencbrownx1@gmail.com>
2022-09-17 06:56:58 -0400

    Add NumberGuess files

---
+++ NumberGuess/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random r = new Random();
+
+int winnum = r.Next(0, 100);
+
+bool win = false;
+
+do
+{
+    Console.Write("Guess a number in-between 0 and 100: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > winnum) { Console.WriteLine("Guess to high..."); ; }
+    else if (num < winnum) { Console.WriteLine("Guess to low..."); ; }
+    else
+    {
+        Console.WriteLine("Correct guess, you win!");
+        win = true;
+    }
+
+} while (win == false);
